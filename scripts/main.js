831ce5860a60d3251586098681f7c2d2f07cc543
Onlymeinnit <okonjie4@gmail.com>
2024-12-15 11:58:06 -0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const menu = document.getElementById("menu-button");
+const menuList = document.getElementById("menu-list");
+const body = document.body;
+
+menu.onclick = () => {
+  menuList.style.height === "0px"
+    ? (menuList.style.height = "285px")
+    : (menuList.style.height = "0px");
+};
+
+body.onscroll = () => {
+  menuList.style.height = "0px";
+};
+
+const welcomeImage = document.getElementById("welcome-image");
+
+function image1() {
+  welcomeImage.setAttribute("src", "full-front-view.jpg");
+}
+
+function image2() {
+  welcomeImage.setAttribute("src", "first-room-image.jpg");
+}
+
+function image3() {
+  welcomeImage.setAttribute("src", "dishes-1.jpg");
+}
+
+function image4() {
+  welcomeImage.setAttribute("src", "3I-bar.jpg");
+}
+
+const images = [image1, image2, image3, image4];
+let index = 0;
+
+function changeImages() {
+  images[index]();
+  index = (index + 1) % images.length;
+
+  setTimeout(changeImages, 5000);
+}
+
+changeImages();
